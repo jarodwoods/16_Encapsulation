1edfa8c20e7142d2ffa0a083e44eeb60213a9510
johndward01 <johndward01@gmail.com>
2021-02-16 17:54:50 -0600

    initial commit

---
+++ Encapsulation_Exercise/Encapsulation01/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation01
+{
+    class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double deposit)
+        {
+            balance += deposit;
+        }
+        public void Withdraw(double withdrawal)
+        {
+            balance -= withdrawal;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+    }
+}
---
+++ Encapsulation_Exercise/Encapsulation01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Encapsulation01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new BankAccount();
+            myAccount.Deposit(1111.11);
+            myAccount.Withdraw(11.11);
+            Console.WriteLine(myAccount.GetBalance());
+        }
+    }
+}
